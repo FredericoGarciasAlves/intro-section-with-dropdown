76d9fe67791233df7c9cfd429da7ab0de6300582
FREDERICO GARCIAS ALVES <garciasalvesfrederico@gmail.com>
2025-05-30 15:29:30 -0300

    mobile

--- script.js
+++ script.js
@@ -2,16 +2,21 @@ const btnFeatures = document.querySelector(".features");
 const btnCompany = document.querySelector(".company");
 
 const featureArrow = document.querySelector(".features-arrow");
-console.log(featureArrow);
 const companyArrow = document.querySelector(".company-arrow");
 
 const listFeatures = document.querySelector(".list-features");
 const listCompany = document.querySelector(".list-company");
 
 const btnHiddenMenu = document.querySelector(".icon-menu-close");
+const btnShowMenu = document.querySelector(".icon-menu");
 const boxMenu = document.querySelector(".box-menu");
 
+let featuresOpen = false;
+let companyOpen = false;
+
 function toogleArrow(e) {
+  if (e.target === btnFeatures && featuresOpen) return;
+  if (e.target === btnCompany && companyOpen) return;
   const featureArrowSrc = featureArrow.getAttribute("src");
   const companyArrowSrc = companyArrow.getAttribute("src");
   if (
@@ -53,4 +58,50 @@ btnHiddenMenu.addEventListener("click", () => {
   setTimeout(() => {
     boxMenu.style.removeProperty("display");
   }, 1000);
+});
+
+const mq = window.matchMedia("(max-width: 376px)");
+
+btnFeatures.addEventListener("click", (e) => {
+  featuresOpen = !featuresOpen;
+  if (featuresOpen) {
+    listFeatures.style.display = "flex";
+    featureArrow.src = "./assets/images/icon-arrow-up.svg";
+    companyOpen = false;
+    listCompany.style.removeProperty("display");
+    companyArrow.src = "./assets/images/icon-arrow-down.svg";
+    if (mq) {
+      btnFeatures.style.paddingBottom = "18.2rem";
+    }
+  } else {
+    listFeatures.style.removeProperty("display");
+    featureArrow.src = "./assets/images/icon-arrow-down.svg";
+    if (mq) {
+      btnFeatures.style.paddingBottom = "0rem";
+    }
+  }
+});
+
+btnCompany.addEventListener("click", () => {
+  companyOpen = !companyOpen;
+  if (companyOpen) {
+    listCompany.style.display = "flex";
+    companyArrow.src = "./assets/images/icon-arrow-up.svg";
+    featuresOpen = false;
+    listFeatures.style.removeProperty("display");
+    featureArrow.src = "./assets/images/icon-arrow-down.svg";
+    if (mq) {
+      btnCompany.style.paddingBottom = "13rem";
+    }
+  } else {
+    listCompany.style.removeProperty("display");
+    companyArrow.src = "./assets/images/icon-arrow-down.svg";
+    if (mq) {
+      btnCompany.style.paddingBottom = "0rem";
+    }
+  }
+});
+
+btnShowMenu.addEventListener("click", () => {
+  boxMenu.style.display = "flex";
 });
